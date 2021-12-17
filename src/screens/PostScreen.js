ea748f89053fa448b134ddf9415983034bb6ebcd
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2021-12-17 13:39:33 +0700

    features/notification: update - push notification

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useEffect} from 'react';
+import {View, Text, Button} from 'react-native';
+import PushNotification from 'react-native-push-notification';
 
 export default function PostScreen() {
-    return (
-        <View>
-            <Text>PostScreen</Text>
-        </View>
-    )
+  useEffect(() => {
+    createChannels();
+  }, []);
+
+  const createChannels = () => {
+    PushNotification.createChannel({
+      channelId: 'test-notification',
+      channelName: 'Test Notification',
+    });
+  };
+  const sendNotification = () => {
+    PushNotification.localNotification({
+      channelId: 'test-notification',
+      title: 'Clicked!',
+      message: "Because I'm very lazy to test it!",
+    });
+  };
+
+  return (
+    <View>
+      <Text>PostScreen</Text>
+      <Button title="Notification" onPress={() => sendNotification()}></Button>
+    </View>
+  );
 }
